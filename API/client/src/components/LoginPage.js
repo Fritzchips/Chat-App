6c5888f74b291bdf11a7234e93e0b86bab91d0ac
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-07-16 19:37:46 -0700

    login modifications

--- API/client/src/components/LoginPage.js
+++ API/client/src/components/LoginPage.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import NewUser from './NewUser';
-import UserSignIn from './UserSignIn';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const LoginPage = () => {
     const [formDisplay, setFormDisplay] = useState('signIn')
 
-    const formHandler = (e) => {
+    const requestHandler = (e) => {
         setFormDisplay(e.target.value);
         console.log('you are', formDisplay);
     }
@@ -15,18 +15,40 @@ const LoginPage = () => {
         console.log("you signed in as guest");
     }
 
+    const formHandler = (e) => {
+        e.preventDefault();
+        console.log(`you ${formDisplay}`)
+    }
+
     return (
         <div>
-            <div>Wecome to Login page</div>
-            <div>
-                {formDisplay === "signIn" ? (<UserSignIn />) : (<NewUser />)}            
-            </div>
-            <button onClick={formHandler} value="signIn">Log In</button>
-            <button onClick={formHandler} value="signUp">Sign Up</button>
+            <h1>Wecome to Twinkle</h1>
+            <div style={{maxWidth: "80%"}}>
+                <div>
+
+                    <Form onSubmit={formHandler}>
+                        <Form.Group className="mb-3" controlId="usernamel">
+                            <Form.Label>User Name:</Form.Label>
+                            <Form.Control type="text" placeholder="Enter user name" required />
+                            <Form.Text className="text-muted"> We'll never share your email with anyone else.</Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control type="password" placeholder="Password" required />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">{formDisplay}</Button>
+                    </Form>
+
+            
+                    <button onClick={requestHandler} value="Sign In">Log In</button>
+                        <button onClick={requestHandler} value="Sign Up">New User</button>
+                 </div>
            
             
 
-            <button onClick={ guestHandler}>Continue as Guest</button>
+                <button onClick={guestHandler}>Continue as Guest</button>
+            </div>
         </div>
     );
 }
--- API/client/src/components/MessageScreen.js
+++ API/client/src/components/MessageScreen.js
@@ -1,25 +1,33 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 
 const MessageScreen = () => {
     const [messages, setMessages] = useState([]);
     return (
 
         <Container className="d-flex flex-column " style={{minHeight: "100%"}} >
-                <div style={{ width: "100%", height: "80vh",  border: "3px solid black", borderRadius: "5px" }}>
-                    <div style={{backgroundColor: "grey"}}><strong>Welcome to this channel</strong></div>
-                    <br></br>
-                    <div>
-                        <p>Fritz</p>
-                        <p>Saying hello there!</p>
-                        <p>Mom</p>
-                        <p>Saying hello there!</p>
-                        <p>Fritz</p>
-                        <p>starting convo</p>
-                    </div>
+            <div style={{ width: "100%", height: "80vh",  border: "3px solid black", borderRadius: "5px" }}>
+                <div style={{backgroundColor: "grey"}}><strong>Welcome to this channel</strong></div>
+                <br></br>
+                <div style={{textAlign: "start"}}>
+                    <p>Fritz</p>
+                    
                 </div>
+            </div>
 
             <div style={{ height: "10vh", width: "100%", marginTop: "20px" }} className="align-self-baseline">
+
+                {/*<InputGroup className="mb-3">*/}
+                {/*    <FormControl*/}
+                {/*        placeholder="Comments"*/}
+                {/*        aria-label="Recipient's username"*/}
+                {/*        aria-describedby="basic-addon2"*/}
+                {/*    />*/}
+                {/*    <Button variant="outline-secondary" id="button-addon2">Post</Button>*/}
+                {/*</InputGroup>*/}
+
                 <input type="text" required style={{width: "80%"}}/>
                 <button>Post</button>
                 </div>
