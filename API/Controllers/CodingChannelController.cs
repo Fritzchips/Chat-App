773ff7baff0d95400ba91ef9757a705b50872027
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-07-17 12:55:38 -0700

    added a few controllers

---
+++ API/Controllers/CodingChannelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class CodingChannelController : BaseApiController
+    {
+       [HttpGet]
+       //get coding channel messages
+
+       [HttpPost]
+       //adding messages to channel
+    }
+}
