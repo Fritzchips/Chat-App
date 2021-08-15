109ac579190ee8e47f2139429dcae9579a764128
Fritzchips <fritzjohn_solibio@yahoo.ca>
2021-08-15 16:48:33 -0700

    setting up for azure deployment

--- API/Startup.cs
+++ API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Infrastructure;
 using API.Authorization.Utilities;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -58,12 +59,6 @@ namespace API
 
             services.AddSingleton<IJwtAuthenticationHandler>(new JwtAuthenticationHandler(_configuration["JwtConfig:Secret"]));
             services.AddSingleton<INhibernateHandler>(new NhibernateHandler());
-           
-
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,8 +67,6 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
             //app.UseHttpsRedirection();
